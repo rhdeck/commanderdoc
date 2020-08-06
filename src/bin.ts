e2c07721d3e01a18f259d5e2189fcabaa50fdb0e
Ray Deck <ray@raydeck.com>
2020-08-06 05:55:59 -0500

    feat: first round of documenting a commander CLI app

--- src/bin.ts
+++ src/bin.ts
@@ -1,3 +1,58 @@
 #!/usr/bin/env node
 import commander from "commander";
+import { commanderToMd, getCommand } from ".";
+import { writeFileSync } from "fs";
+import { resolve } from "path";
+commander
+
+  .command("markdown <sourcefile>")
+  .option(
+    "-e --exported <commanderObject>",
+    "Name of the exported commander object to inspect",
+    "commander"
+  )
+  .option(
+    "-o --out-file <outfile>",
+    "File to store markdown output (defaults to stdout)"
+  )
+  .option("-n --cli-name <name>", "Name of the executable")
+  .action((sourcefile, { exported, outFile, cliName = sourcefile }) => {
+    const resolvedSource = resolve(process.cwd(), sourcefile);
+    const exports = require(resolvedSource);
+    const command = exports[exported];
+    if (!command) {
+      console.error("no export found at ", exported);
+      process.exit(1);
+    }
+    const markdown = commanderToMd(command, cliName);
+    if (outFile) writeFileSync(outFile, markdown);
+    else process.stdout.write(markdown);
+  });
+commander
+  .command("json <sourcefile>")
+  .description("Extract inspected commander as JSON")
+  .option(
+    "-e --exported <commanderObject>",
+    "Name of the exported commander object to inspect",
+    "commander"
+  )
+  .option(
+    "-o --out-file <outfile>",
+    "File to store markdown output (defaults to stdout)"
+  )
+  .action((sourceFile, { exported, outFile }) => {
+    const resolvedSource = resolve(process.cwd(), sourceFile);
+    const exports = require(resolvedSource);
+    const command = exports[exported];
+    if (!command) {
+      console.error("no export found at ", exported);
+      process.exit(1);
+    }
+    const obj = getCommand(command);
+    const json = JSON.stringify(obj, null, 2);
+    if (outFile) writeFileSync(outFile, json);
+    else process.stdout.write(json);
+  });
+
 commander.parse(process.argv);
+export { commander };
---
+++ src/index.ts
@@ -0,0 +1,138 @@
+import { join } from "path";
+
+export function getOptions(options: any[]) {
+  return options.filter(Boolean).map(getOption);
+}
+function getOption(o: { [key: string]: any }) {
+  const {
+    flags,
+    required,
+    optional,
+    mandatory,
+    variadic,
+    short,
+    long,
+    negate,
+    description,
+    defaultValue,
+    args,
+  } = o;
+  return {
+    flags,
+    required,
+    optional,
+    mandatory,
+    variadic,
+    short,
+    long,
+    negate,
+    description,
+    defaultValue,
+    args,
+  };
+}
+type OptionType = ReturnType<typeof getOption>;
+type CommandType = {
+  name: string;
+  description?: string;
+  options?: OptionType[];
+  commands?: CommandType[];
+  args?: { required: boolean; name: string; variadic: boolean }[];
+};
+export function getCommands(commands: { [key: string]: any }[]): CommandType[] {
+  return commands.filter(Boolean).map(getCommand);
+}
+export function getCommand(o: { [key: string]: any }): CommandType {
+  const {
+    _args: args,
+    _name: name,
+    _description: description,
+    options,
+    commands,
+  } = o;
+  return {
+    name,
+    args,
+    description,
+    options: options && getOptions(options),
+    commands: commands && getCommands(commands),
+  };
+}
+function padLevel(level: number) {
+  return new Array(level).fill("#").join("");
+}
+function optionToMd(option: OptionType, startLevel: number = 1) {
+  const { flags, description, defaultValue } = option;
+  let lines: string[] = [];
+  //markdown - start with header
+  lines.push(
+    `* ${flags.replace("<", "\\<`").replace(">", "`>")} ${description} ${
+      defaultValue && "(default: " + defaultValue.toString()
+    }`
+  );
+  return lines.join("\n");
+}
+export function commandToMd(command: CommandType, startLevel: number = 1) {
+  const { options, description, name, commands, args } = command;
+  let lines: string[] = [];
+  lines.push(
+    padLevel(startLevel) +
+      " " +
+      name +
+      (args &&
+        !!args!.length &&
+        " " +
+          args!
+            .map(({ required, name, variadic }) => {
+              const pieces: string[] = [];
+              pieces.push(required ? "\\<" : "[");
+              pieces.push("`");
+              if (variadic) pieces.push("...");
+              pieces.push(name);
+              pieces.push("`");
+              pieces.push(required ? ">" : "]");
+              return pieces.join("");
+            })
+            .join(" ")) || ""
+  );
+  if (description) lines.push(description);
+  if (options) {
+    lines.push(...options.map((option) => optionToMd(option)));
+  }
+  if (commands && commands.filter(Boolean).length) {
+    lines.push(padLevel(startLevel + 1) + " Subcommands");
+    lines.push(
+      ...commands.map((command) => commandToMd(command, startLevel + 2))
+    );
+  }
+  return lines.join("\n");
+}
+export function commanderToMd(
+  commander: { [key: string]: any },
+  cliName: string,
+  startLevel: number = 1
+) {
+  const inspectedCommander = getCommand(commander);
+  let lines: string[] = [];
+  lines.push(padLevel(startLevel) + " Usage");
+  lines.push("```bash");
+  if (
+    inspectedCommander.commands &&
+    inspectedCommander.commands.filter(Boolean).length
+  )
+    lines.push(cliName + " [options] [command]");
+  else lines.push(cliName + "[options]");
+  lines.push("```");
+  if (
+    inspectedCommander.commands &&
+    inspectedCommander.commands.filter(Boolean).length
+  ) {
+    lines.push(padLevel(startLevel + 1) + " Commands");
+    lines.push(
+      ...inspectedCommander.commands.map((command) =>
+        commandToMd(command, startLevel + 2)
+      )
+    );
+  } else lines.push(commandToMd(inspectedCommander, startLevel + 1));
+  return lines.join("\n");
+}
